3838f6c128621d7770799feb38f786762da6b1c8
Alexander Glukhov <a2gil@mail.ru>
2018-01-23 01:27:27 +0300

    commit

--- src/app/i-text/i-text.component.ts
+++ src/app/i-text/i-text.component.ts
@@ -8,7 +8,8 @@ import { Component, OnInit,Input } from '@angular/core';
 export class ITextComponent implements OnInit {
 
   @Input() public img: string = '';
-
+  @Input() public width: number;
+  
   constructor() { }
 
   ngOnInit() {
